fixed spin calculation

nanodevice_k_qd.py
@@ -3,52 +3,69 @@ import utils_tb
 
 import utils_tb_k as utils
 
+# lets define parameters of full TB model here and cross-check with MB Matlab code
+lattice_const = 0.3323 #in nm
+d2_up         = 0.1669 #in nm
+d2_down       = 0.1680 #in nm
+d0            = 0.6400 #in nm
 
-lattice_const = 0.3323  # now in nm
-# --- Slater-Koster layers parameters
+lambda_M_up    =  0.093
+lambda_M_down  =  0.236
+lambda_X2_up   =  0.175
+lambda_X2_down = -0.275
+
 # layer up: MoSe2
-Ed_up          = -0.09
-Ep1_up         = -5.01
-Ep0_up         = -5.30
-Vdp_sigma_up   = -3.08
-Vdp_pi_up      =  1.08
-Vdd_sigma_up   = -0.94
-Vdd_pi_up      =  0.75
-Vdd_delta_up   =  0.13
-Vpp_sigma_up   =  1.39
-Vpp_pi_up      = -0.45
-Ep1_odd_up     =  Ep1_up
-Ep0_odd_up     =  Ep0_up
-Ed_odd_up      =  Ed_up
-lambda_M_up    =  0.186/2.  # 0.15  # it should be: 0.186d0/2.0d0
-lambda_X2_up   =  0.200/2.  # 0.15  # it should be: 0.200d0/2.0d0
-# layer down: WSe2
-Ed_down        = -0.12
-Ep1_down       = -4.17
-Ep0_down       = -4.52
-Vdp_sigma_down = -3.31
-Vdp_pi_down    =  1.16
-Vdd_sigma_down = -1.14
-Vdd_pi_down    =  0.72
-Vdd_delta_down =  0.17
-Vpp_sigma_down =  1.16
-Vpp_pi_down    = -0.25
-Ep1_odd_down   =  Ep1_down
-Ep0_odd_down   =  Ep0_down
-Ed_odd_down    =  Ed_down 
-lambda_M_down  =  0.472/2.  # 0.35  # it should be: 0.472d0/2.0d0
-lambda_X2_down = -0.390/2.  # -0.20  # it should be: -0.390d0/2.0d0
-# interlayer
-Vpp_sigma_inter =  0.5  # positive
-Vpp_pi_inter    = -0.5  # negative
-Vdd_sigma_inter = -0.3  # negative
-Vdd_pi_inter    =  0.3  # positive
-Vdd_delta_inter = -0.6  # negative
+Ed_up          = -0.140
+Ep1_up         = -5.060
+Ep0_up         = -5.268
+Vdp_sigma_up   = -2.980
+Vdp_pi_up      =  1.073
+Vdd_sigma_up   = -1.028
+Vdd_pi_up      =  0.838
+Vdd_delta_up   =  0.229
+Vpp_sigma_up   =  1.490
+Vpp_pi_up      = -0.550
+Ep1_odd_up     = -5.051
+Ep0_odd_up     = -5.236
+Ed_odd_up      = -0.112
 
-material = utils_tb.Newmaterial(lattice_const,
+# layer down: WSe2
+Ed_down        = -0.220
+Ep1_down       = -4.234
+Ep0_down       = -4.620
+Vdp_sigma_down = -3.251
+Vdp_pi_down    =  1.060
+Vdd_sigma_down = -1.240
+Vdd_pi_down    =  0.817
+Vdd_delta_down =  0.255
+Vpp_sigma_down =  1.258
+Vpp_pi_down    = -0.350
+Ep1_odd_down   = -4.132
+Ep0_odd_down   = -4.554
+Ed_odd_down    = -0.032
+
+# interlayer
+Vpp_sigma_inter =  1.393
+Vpp_pi_inter    = -0.012
+Vdd_sigma_inter =  0.196
+Vdd_pi_inter    = -0.480
+Vdd_delta_inter =  0.091
+
+Vdp_sigma_inter  = -0.044;
+Vpd_sigma_inter  = -1.344;
+Vdp_pi_inter     =  0.411;
+Vpd_pi_inter     = -1.780;
+
+offset          = 0.0
+
+material = utils_tb.Newmaterial(lattice_const, d2_up, d2_down, d0,
                     Ed_up,Ep1_up,Ep0_up,Vdp_sigma_up,Vdp_pi_up,Vdd_sigma_up,Vdd_pi_up,Vdd_delta_up,Vpp_sigma_up,Vpp_pi_up,Ep1_odd_up,Ep0_odd_up,Ed_odd_up,lambda_M_up,lambda_X2_up,
                     Ed_down,Ep1_down,Ep0_down,Vdp_sigma_down,Vdp_pi_down,Vdd_sigma_down,Vdd_pi_down,Vdd_delta_down,Vpp_sigma_down,Vpp_pi_down,Ep1_odd_down,Ep0_odd_down,Ed_odd_down,lambda_M_down,lambda_X2_down,
-                    Vpp_sigma_inter,Vpp_pi_inter,Vdd_sigma_inter,Vdd_pi_inter,Vdd_delta_inter, 0.)
+                    Vpp_sigma_inter,Vpp_pi_inter,Vdd_sigma_inter,Vdd_pi_inter,Vdd_delta_inter,Vdp_sigma_inter,Vpd_sigma_inter,Vdp_pi_inter,Vpd_pi_inter,
+                    offset)
+
+#material.set_efield(0.)  # in mV/nm
+material.set_efield(1000.)  # in mV/nm
 
 k_path = utils_tb.load_k_path('kpointsDFT.dat')
 lattice = utils_tb.Lattice(BZ_path=k_path)
@@ -56,13 +73,12 @@ lattice.select_k_indices(distance=4)
 model = utils_tb.BandModel(material, lattice)
 eigen_solver = utils_tb.EigenSolver(model)
 plotting = utils_tb.Plotting(eigen_solver.model)
-parameters = np.array([0.030411386321679234, -0.06768325945268493, -0.13985445526998835, 0.10354798970965703, 0.09511473252137233, 0.017192102201147058, 0.0009731466399938161, 0.09503724114298144, 0.006517343991442069, -0.01765029376948105, -0.139991453790443, 0.022210650586162457, -0.1399665270159982, 0.1396462854912086, -0.01873315173927232, -0.13965675237116856, 0.0479373707384002, -0.1399412022063997, -0.08320015408731994, 0.13994380829292616, 0.06816633552643377, -0.0807977909651734, -0.11578177646634323, -0.13999999976336608, -0.1395760909761852, -0.1398140425616252, -1.1006155026388882, -0.1546785496916816, -0.4117617630474648, 1.8318187602187703, -0.329984241003078, -0.21562176129413912])
+
+#parameters = np.array([0.030411386321679234, -0.06768325945268493, -0.13985445526998835, 0.10354798970965703, 0.09511473252137233, 0.017192102201147058, 0.0009731466399938161, 0.09503724114298144, 0.006517343991442069, -0.01765029376948105, -0.139991453790443, 0.022210650586162457, -0.1399665270159982, 0.1396462854912086, -0.01873315173927232, -0.13965675237116856, 0.0479373707384002, -0.1399412022063997, -0.08320015408731994, 0.13994380829292616, 0.06816633552643377, -0.0807977909651734, -0.11578177646634323, -0.13999999976336608, -0.1395760909761852, -0.1398140425616252, -1.1006155026388882, -0.1546785496916816, -0.4117617630474648, 1.8318187602187703, -0.329984241003078, -0.21562176129413912])
 # new Kasia
-parameters = np.array([-5.958861367832076E-002, -5.07768325945268, -5.43985445526999, -2.97645201029034, 1.17511473252137, -0.922807897798853, 0.750973146639994, 0.225037241142981, 1.39651734399144, -0.467650293769481, -5.21767471324313, -5.41764380468383, -0.199555140694319, 1.964628549120861E-002, -4.18873315173927, -4.65965675237117, -3.26206262926160, 1.02005879779360, -1.22320015408732, 0.859943808292926, 0.238166335526434, 1.07920220903483, -0.365781776466343, -4.32873315150264, -4.79923284334735, -0.120167757070417, -1.10061550263889, -0.154678549691682, -0.500000000000000, 1.83181876021877, -0.329984241003078])
+#parameters = np.array([-5.958861367832076E-002, -5.07768325945268, -5.43985445526999, -2.97645201029034, 1.17511473252137, -0.922807897798853, 0.750973146639994, 0.225037241142981, 1.39651734399144, -0.467650293769481, -5.21767471324313, -5.41764380468383, -0.199555140694319, 1.964628549120861E-002, -4.18873315173927, -4.65965675237117, -3.26206262926160, 1.02005879779360, -1.22320015408732, 0.859943808292926, 0.238166335526434, 1.07920220903483, -0.365781776466343, -4.32873315150264, -4.79923284334735, -0.120167757070417, -1.10061550263889, -0.154678549691682, -0.500000000000000, 1.83181876021877, -0.329984241003078])
 #material.update_parameters(*parameters)
-material.set_parameters(*parameters)
-material.set_efield(1000.)  # in mV/nm
-#material.set_efield(1.)  # in mV/nm
+#material.set_parameters(*parameters)
 
 # define flake    
 flake_edge_size = [102,102]  # [800,800] 
@@ -79,7 +95,7 @@ flake.create_reciprocal_lattice()
 print("constructing plane-wave basis...")
 basis_k = utils.Planewaves(flake, model)
 #basis_k.select_subspace(flake.K_points, 1.5)
-basis_k.select_subspace(flake.Q_points, 2.5)  # 14.
+basis_k.select_subspace(flake.Q_points, 1.5)  # 14.
 #basis_k.select_subspace_half()
 plot_flake.plot_flake_lattice_k(subsets=basis_k.subspaces)
 
@@ -96,7 +112,7 @@ for i, n in enumerate(flake.nodes):
     #flake.potential[i] += np.exp(-d**2/(3./utils.au.Ah)**2/2.)*1./utils.au.Eh
     #flake.potential[i] -= (np.exp(-((x-6.8/utils.au.Ah)**2+(y+4.6/utils.au.Ah)**2)/(5./utils.au.Ah)**2/2.)-1.)*250./utils.au.Eh
     #flake.potential[i] = (np.exp(-(x**2+y**2)/(5./utils.au.Ah)**2/2.)-1.)*250./utils.au.Eh
-    flake.potential[i] = (np.exp(-(x**2+y**2)/(5./utils.au.Ah)**2/2.)-1.)*0./utils.au.Eh
+    flake.potential[i] = (np.exp(-(x**2+y**2)/(5./utils.au.Ah)**2/2.)-1.)*100./utils.au.Eh
 
 # Q1'-Q3 coupling:
 # angle = -np.pi/6.
@@ -146,7 +162,7 @@ eigenvalues, eigenvectors = es.solve_eigenproblem_arpack_dense(hamiltonian,
                                 reverse = False,
                                 calculate_eigenvectors = True)
 
-print(eigenvalues)
+print(eigenvalues[:10]*utils.au.Ehh)
 plot_bands.plot_eigenvalues(eigenvalues)
 np.save('./results/eigenvalues', eigenvalues, allow_pickle=True)
 

utils_tb_k.py
@@ -62,6 +62,7 @@ class AtomicUnits:
     """
     # atomic units
     Eh=27211.4 # meV
+    Ehh=Eh/1000. # eV
     Ah=0.05292 # nm
     Th=2.41888e-5 # ps
     Bh=235051.76 # Teslas
@@ -1064,8 +1065,9 @@ class FlakeMethods:
         if calculate_real_states:
             states = np.zeros((no_of_eigenstates, self.flake.no_of_nodes*self.m.dim), dtype=np.complex128)   
         #
-        k1 = self.basis_k.subspaces[0].shape[0]*dim_r  # K-valley range
-        k2 = k1 + self.basis_k.subspaces[1].shape[0]*dim_r  # K'-valley range
+        # fast fix to have spin values reasonable:
+        k1 = 0  #k1 = self.basis_k.subspaces[0].shape[0]*dim_r  # K-valley range
+        k2 = len(eigenvectors[:,0])  #k2 = k1 + self.basis_k.subspaces[1].shape[0]*dim_r  # K'-valley range
         for i in range(no_of_eigenstates):
             j = 0
             jj= 0
@@ -1379,7 +1381,7 @@ class PlottingBands:
         fig, ax = plt.subplots(figsize=(fx,fy), dpi=dpi)
         ax.set_xlabel("subsequent eigenstates")
         ax.set_ylabel("energy (eV)")        
-        y=eigenvalues
+        y=eigenvalues*au.Ehh
         dy=np.sqrt(pointsize)*dpi/72.*(np.amax(y)-np.amin(y))/(fy*dpi)/1.5
         x=np.arange(1, eigenvalues.size+1)
         sv = ax.scatter(x=np.arange(1, eigenvalues.size+1), y=y+dy, 
@@ -1399,7 +1401,7 @@ class PlottingBands:
         ax.set_xlabel('$k_x$ (nm$^{-1}$)')
         ax.set_ylabel("energy (eV)")   
         #ax.set_ylim([-79,-5])     
-        y=eigenvalues
+        y=eigenvalues*au.Ehh
         dy=np.sqrt(pointsize)*dpi/72.*(np.amax(y)-np.amin(y))/(fy*dpi)/1.7
         #ax.plot([self.flake.K_points[0][1]/au.Ah,self.flake.K_points[0][1]/au.Ah], [-80,0], '--', c='black', zorder=1)
         #ax.plot([self.flake.K_points[1][1]/au.Ah,self.flake.K_points[1][1]/au.Ah], [-80,0], '--', c='black', zorder=1)
@@ -1423,7 +1425,7 @@ class PlottingBands:
         ax.set_xlabel('$k_y$ (nm$^{-1}$)')
         ax.set_ylabel("energy (eV)")   
         #ax.set_ylim([-79,-5])     
-        y=eigenvalues
+        y=eigenvalues*au.Ehh
         dy=np.sqrt(pointsize)*dpi/72.*(np.amax(y)-np.amin(y))/(fy*dpi)/1.5
         #ax.plot([self.flake.K_points[0][1]/au.Ah,self.flake.K_points[0][1]/au.Ah], [-80,0], '--', c='black', zorder=1)
         #ax.plot([self.flake.K_points[1][1]/au.Ah,self.flake.K_points[1][1]/au.Ah], [-80,0], '--', c='black', zorder=1)
@@ -1446,7 +1448,7 @@ class PlottingBands:
         ax.set_xlabel('$k_x$ (nm$^{-1}$)')
         ax.set_ylabel("energy (eV)")   
         ax.set_ylim([-79,-5])     
-        y=eigenvalues
+        y=eigenvalues*au.Ehh
         ax.plot([self.flake.K_points[0][0]/au.Ah,self.flake.K_points[0][0]/au.Ah], [-80,0], '--', c='black', zorder=1)
         ax.plot([self.flake.K_points[1][0]/au.Ah,self.flake.K_points[1][0]/au.Ah], [-80,0], '--', c='black', zorder=1)
         sv = ax.scatter(x=k_max[:,0]/au.Ah, y=y, 
@@ -1466,8 +1468,8 @@ class PlottingBands:
         subset = np.concatenate(subsets)
         energysurface = ax.scatter(x=subset[:,0]/au.Ah, 
                         y=subset[:,1]/au.Ah, 
-                        c=energy,  # *au.Eh,
-                        s=self.pointsize/2., cmap='coolwarm')
+                        c=energy*au.Ehh,
+                        s=self.pointsize/2., cmap='coolwarm')  #, vmin=1.01, vmax=1.02)
         cbar = fig.colorbar(energysurface, ax=ax)
         cbar.set_label(r'energy (eV)')
         plt.savefig(os.path.join(self.directory, filename), bbox_inches='tight', dpi=200)
@@ -1535,7 +1537,7 @@ class PlottingBands:
         k_path = np.linspace(0., 1., num=self.grid_k.shape[0])
         
         for band_idx in range(Ek.shape[1]):
-            ax.scatter(k_path, Ek[:,band_idx], s=pointsize, marker='.')
+            ax.scatter(k_path, Ek[:,band_idx]*au.Ehh, s=pointsize, marker='.')
 
         handles, labels = plt.gca().get_legend_handles_labels()
         by_label = dict(zip(labels, handles))